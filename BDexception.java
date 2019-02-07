e2bec9ae5795988e8ae04963e3d4a5162bf8ecf0
ValerinaL <47216767+ValerinaL@users.noreply.github.com>
2019-02-07 17:12:47 +0100

    Create BDexception.java

---
+++ BDexception.java
@@ -0,0 +1,16 @@
+package DAL;
+
+/**
+ *
+ * @author Admin
+ */
+public class BDexception extends Exception{
+
+    /**
+     *
+     * @param msg
+     */
+    public BDexception (String msg){
+       super(msg);
+    }
+}
